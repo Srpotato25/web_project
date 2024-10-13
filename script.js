7d7872673ddc97b4f24084b0bb442f7605a4f9a4
David Castillo Mora <davidcm2803@gmail.com>
2024-10-12 19:30:10 -0600

    js login

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Importaciones de Firebase
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
 import { 
