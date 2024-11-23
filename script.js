155e276e1448beae72b6c9eee0813bd423e42ec7
Srpotato25 <danielseqh@hotmail.com>
2024-11-23 08:59:16 -0600

    Carousel de imagenes

--- script.js
+++ script.js
@@ -68,7 +68,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
 new Swiper(".card-wrapper", {
     loop: true,
     spaceBetween: 30,
@@ -93,3 +92,47 @@ new Swiper(".card-wrapper", {
     },
   });
 
+//Programacion del carousel 
+
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
+
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRigt())
+
+setInterval(() => {
+    moveToRigt()
+}, 6000);
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRigt() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s"
+}
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`
+        return;
+    }
+    operacion = operacion - widthImg
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s"
+}
